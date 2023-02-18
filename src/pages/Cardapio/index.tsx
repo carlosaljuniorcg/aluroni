1b105f4a031e1cdc7cc85aa19fc2c30106c406e9
Carlos Antônio <105462756+carlosaljuniorcg@users.noreply.github.com>
2023-02-18 15:34:56 -0300

    Update 18022023

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -17,7 +17,7 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A casa do código e da massa
+          Codando Massas
         </div>
       </header>
       <section className={styles.cardapio}>
